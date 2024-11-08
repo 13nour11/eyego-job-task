398c84949e0de4666a71259f4ef81136d414ecd4
Nourhan Fahmy <nourhanfahmy010@gmail.com>
2024-11-08 21:29:46 +0200

    last deploy

--- app/page.js
+++ app/page.js
@@ -11,34 +11,33 @@ export default function Home() {
   const [isClient, setIsClient] = useState(false);
   const [editTask, setEditTask] = useState(null); // Track task being edited
 
+  const TASKS_KEY = "tasks";
+
   const toggleTaskCompletion = (taskId) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
-      )
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
     );
+    setTasks(updatedTasks);
   };
 
   const addOrUpdateTask = (taskTitle) => {
     if (editTask) {
       // Update existing task
-      setTasks(
-        tasks.map((task) =>
-          task.id === editTask.id ? { ...task, title: taskTitle } : task
-        )
+      const updatedTasks = tasks.map((task) =>
+        task.id === editTask.id ? { ...task, title: taskTitle } : task
       );
+      setTasks(updatedTasks);
       setEditTask(null); // Reset edit mode
     } else {
       // Add new task
-      setTasks([
-        ...tasks,
-        { id: uuidv4(), title: taskTitle, completed: false },
-      ]);
+      const newTask = { id: uuidv4(), title: taskTitle, completed: false };
+      setTasks([...tasks, newTask]);
     }
   };
 
   const deleteTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
   };
 
   const filteredTasks = tasks.filter((task) => {
@@ -53,33 +52,41 @@ export default function Home() {
 
   useEffect(() => {
     setIsClient(true);
-    if (tasks.length === 0) {
+    // Load tasks from localStorage
+    const storedTasks = JSON.parse(localStorage.getItem(TASKS_KEY)) || [];
+    if (storedTasks.length > 0) {
+      setTasks(storedTasks);
+    } else {
       setTasks([{ id: uuidv4(), title: "Sample Task", completed: false }]);
     }
   }, []);
-  
+
+  useEffect(() => {
+    // Save tasks to localStorage whenever they change
+    localStorage.setItem(TASKS_KEY, JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    // #906387
     <div className="bg-cyan-950 min-h-screen py-8">
-    {isClient && (
-      <div className="max-w-xl mx-auto p-6 bg-indigo-100 rounded-lg shadow-lg ">
-        <h1 className="text-2xl font-bold text-center text-cyan-800 mb-6">
-          Task Manager
-        </h1>
-        <TaskForm
-          addOrUpdateTask={addOrUpdateTask}
-          task={editTask || {}}
-          editTask={editTask}
-        />
-        <FilterButtons setFilter={setFilter} />
-        <TaskList
-          filteredTasks={filteredTasks}
-          toggleTaskCompletion={toggleTaskCompletion}
-          deleteTask={deleteTask}
-          startEditing={startEditing}
-        />
-      </div>
-    )}
+      {isClient && (
+        <div className="max-w-xl mx-auto p-6 bg-indigo-100 rounded-lg shadow-lg ">
+          <h1 className="text-2xl font-bold text-center text-cyan-800 mb-6">
+            Task Manager
+          </h1>
+          <TaskForm
+            addOrUpdateTask={addOrUpdateTask}
+            task={editTask || {}}
+            editTask={editTask}
+          />
+          <FilterButtons setFilter={setFilter} />
+          <TaskList
+            filteredTasks={filteredTasks}
+            toggleTaskCompletion={toggleTaskCompletion}
+            deleteTask={deleteTask}
+            startEditing={startEditing}
+          />
+        </div>
+      )}
     </div>
   );
 }
